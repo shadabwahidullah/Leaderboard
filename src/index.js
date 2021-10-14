8af80f4c70f4cd95da169d4083985b93e382d537
Wahidullah Shadab <shadabwahidullah@gmail.com>
2021-10-15 02:31:15 +0430

    Resolve linters errors

--- src/index.js
+++ src/index.js
@@ -1,55 +1,54 @@
-import _, { result, some } from "lodash"; // eslint-disable-line no-unused-vars
-import "./style.css";
+import _, { result, some } from 'lodash'; // eslint-disable-line no-unused-vars
+import './style.css';
 
-const submit = document.getElementById("submit");
-const refresh = document.getElementById("refresh");
-const scoreslist = document.getElementById("scores");
+const submit = document.getElementById('submit');
+const refresh = document.getElementById('refresh');
+const scoreslist = document.getElementById('scores');
 
-const baseURL =
-  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/battlefield/scores/";
-
-function printScores(scores) {
-  scoreslist.innerHTML = "";
-  scores.forEach((element) => {
-    createHTML(element["user"], element["score"]);
-  });
-}
+const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/battlefield/scores/';
 
 function createHTML(name, score) {
-  const listItem = document.createElement("li");
-  listItem.classList.add("p-3", "fw-bold");
-  listItem.innerHTML = name + ": " + score;
+  const listItem = document.createElement('li');
+  listItem.classList.add('p-3', 'fw-bold');
+  listItem.innerHTML = `${name}: ${score}`;
   scoreslist.appendChild(listItem);
 }
 
+function printScores(scores) {
+  scoreslist.innerHTML = '';
+  scores.forEach((element) => {
+    createHTML(element.user, element.score);
+  });
+}
+
 async function getScores() {
-  const response = await fetch(baseURL).then(function (response) {
-    response.text().then(function (text) {
+  await fetch(baseURL).then((response) => {
+    response.text().then((text) => {
       printScores(JSON.parse(text).result);
     });
   });
 }
 
 async function postScore(name, score) {
-  let newscore = { user: name, score: score };
+  const newscore = { user: name, score };
 
   await fetch(baseURL, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify(newscore),
     headers: {
-      "Content-type": "application/json; charset=UTF-8",
+      'Content-type': 'application/json; charset=UTF-8',
     },
   });
 }
 
 getScores();
 
-refresh.addEventListener("click", getScores);
-submit.addEventListener("click", (event) => {
+refresh.addEventListener('click', getScores);
+submit.addEventListener('click', (event) => {
   event.preventDefault();
-  const name = document.getElementById("new-name");
-  const score = document.getElementById("new-score");
+  const name = document.getElementById('new-name');
+  const score = document.getElementById('new-score');
   postScore(name.value, score.value);
-  name.value = "";
-  score.value = "";
+  name.value = '';
+  score.value = '';
 });
