69b6c797a15fe4ec1e94877e98536de224cbc722
Wahidullah Shadab <shadabwahidullah@gmail.com>
2021-10-14 19:10:35 +0430

    Resolve linters errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
-import _ from 'lodash';
+import _ from 'lodash'; // eslint-disable-line no-unused-vars
 import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
